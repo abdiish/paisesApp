04a9c452061c6b23d9f656ae1e5f5c7cb11b611a
Alan Cortes <ing.ti.upmh@gmail.com>
2021-07-07 17:27:03 -0500

    1.- El contenido de sidebar en app.component.html se mueve aal archivo   sidebar.component.html. 2.- En el archivo sidebar.component.ts se agrega un template literal   para agregar un estilo css al elemnto li 3.- En shared.module.ts se hace un import de RouterModule, esto con la   finalidad de poder hacer uso de RouterLink.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,7 +13,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent //Se exporta para poder usarlo en app.component
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
